37c5a042c91da2a32bb5afdaa7c6b793e6ee6c60
Carlos <hola@cafeconcarlos.com>
2024-04-30 10:38:59 +0200

    hero done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
+import { HeroComponent } from './components/hero/hero.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/hero/hero.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'hero',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+
+
+
+
+}
+
